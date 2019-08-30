202689751fcddad9fd241030790fb1c637b0ec6f
Leozera <leobhal13@gmail.com>
2019-08-30 13:09:58 -0300

    Copntinua;'ao da nossa Api

--- ApiRestUploads/backend/src/index.js
+++ ApiRestUploads/backend/src/index.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+
 const app = express();
+
+/**
+ * DataBase Setup
+ */
+const uri = "mongodb+srv://DbApiNode:CodeZera2019@cluster0-84qfi.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.connect(uri,
+    {
+        useNewUrlParser: true
+    }
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- ApiRestUploads/backend/src/routes.js
+++ ApiRestUploads/backend/src/routes.js
@@ -2,13 +2,21 @@ const routes = require('express').Router();
 const multer = require('multer');
 const multerConfig = require('./config/multer');
 
+const Post = require('./models/Post');
 
 
-routes.post('/posts', multer(multerConfig).single('file'), (req, res) => {
-    console.log(req.file);
 
+routes.post('/posts', multer(multerConfig).single('file'), async (req, res) => {
+    const { originalname: name, size, filename: key } = req.file;
 
-    return res.json({ hello: "Leozera!" })
+    const post = await Post.create({
+        name,
+        size,
+        key,
+        url: '',
+    });
+
+    return res.json(post)
 });
 
 module.exports = routes;
