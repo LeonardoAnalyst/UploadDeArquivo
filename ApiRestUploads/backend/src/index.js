58e2bbbf57c983dd43807161ccb6c6c00a7cf7a2
Leozera <leobhal13@gmail.com>
2019-09-02 11:31:54 -0300

    Fianalizando a Api

--- ApiRestUploads/backend/src/index.js
+++ ApiRestUploads/backend/src/index.js
@@ -1,13 +1,16 @@
+require('dotenv').config();
+
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 
 /**
  * DataBase Setup
  */
-const uri = "mongodb+srv://DbApiNode:CodeZera2019@cluster0-84qfi.mongodb.net/test?retryWrites=true&w=majority";
+process.env.MONGO_URL,
 mongoose.connect(uri,
     {
         useNewUrlParser: true
@@ -17,7 +20,9 @@ mongoose.connect(uri,
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
-
+app.use('/files', 
+express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+);
 
 app.use(require('./routes'));
 
